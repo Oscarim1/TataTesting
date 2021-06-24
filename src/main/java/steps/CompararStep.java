8d3e3fe960ff716c1798aad235733d11190a38d1
Oscar Reyes <ooreyes.ro@gmail.com>
2021-06-24 19:20:55 -0400

    Commit inicial, creado el primer feature y la estructura del proyeto basada en page object

---
+++ src/main/java/steps/CompararStep.java
@@ -0,0 +1,45 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CompararStep {
+	@Given("abrir la pagina de lenovo")
+	public void abrir_la_pagina_de_lenovo() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("seleccionar primera laptop")
+	public void seleccionar_primera_laptop() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@And("agregar a la comparacion la primera laptop")
+	public void agregar_a_la_comparacion_la_primera_laptop() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("seleccionar la segunda laptop")
+	public void seleccionar_la_segunda_laptop() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@And("agregar a la comparacion la segunda laptop")
+	public void agregar_a_la_comparacion_la_segunda_laptop() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("obtener datos de la especificaciones")
+	public void obtener_datos_de_la_especificaciones() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
