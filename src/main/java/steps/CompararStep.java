9a211838abc89caeacd233b34af3ece143e35615
Oscar Reyes <ooreyes.ro@gmail.com>
2021-06-24 20:57:40 -0400

    Dependencias, primer test listo

--- src/main/java/steps/CompararStep.java
+++ src/main/java/steps/CompararStep.java
@@ -1,45 +1,54 @@
 package steps;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CompararStep {
-	@Given("abrir la pagina de lenovo")
-	public void abrir_la_pagina_de_lenovo() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	WebDriver driver = null;
+	@Given("abrir el navegador")
+	public void abrir_el_navegador() {
+		String projectPath = System.getProperty("user.dir");
+	    System.setProperty("webdriver.chrome.driver", projectPath+"/src/test/resources/drivers/chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	    driver.manage().window().maximize();
 	}
 
-	@When("seleccionar primera laptop")
+	@When("ir a la pagina de lenovo")
+	public void ir_a_la_pagina_de_lenovo() {
+	    
+	}
+
+	@And("seleccionar primera laptop")
 	public void seleccionar_primera_laptop() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
-	@And("agregar a la comparacion la primera laptop")
+	@When("agregar a la comparacion la primera laptop")
 	public void agregar_a_la_comparacion_la_primera_laptop() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
-	@When("seleccionar la segunda laptop")
+	@And("seleccionar la segunda laptop")
 	public void seleccionar_la_segunda_laptop() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
-	@And("agregar a la comparacion la segunda laptop")
+	@When("agregar a la comparacion la segunda laptop")
 	public void agregar_a_la_comparacion_la_segunda_laptop() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   
 	}
 
 	@Then("obtener datos de la especificaciones")
 	public void obtener_datos_de_la_especificaciones() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
-
 }
--- src/test/resources/Features/comparacion.feature
+++ src/test/resources/Features/comparacion.feature
@@ -1,9 +1,11 @@
+@Ciclo1
 Feature: Realizar la comparacion de dos laptops
 
   Scenario: Validar comparacion de laptops
-    Given abrir la pagina de lenovo
-    When seleccionar primera laptop
-    And agregar a la comparacion la primera laptop
-    When seleccionar la segunda laptop
-    And agregar a la comparacion la segunda laptop
+  	Given abrir el navegador
+    When ir a la pagina de lenovo
+    And seleccionar primera laptop
+    When agregar a la comparacion la primera laptop
+    And seleccionar la segunda laptop
+    When agregar a la comparacion la segunda laptop
     Then obtener datos de la especificaciones
